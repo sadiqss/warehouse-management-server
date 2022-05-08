c03bf66a1be6157cfb51bad643278366a688ca71
sadiqss <sadiq.sau48@gmail.com>
2022-05-08 08:57:19 +0100

    add,delete

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -21,6 +22,15 @@ async function run() {
         await client.connect();
         const productCollection = client.db('inventorydb').collection('items');
 
+        //auth
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '60d'
+            });
+            res.send({ accessToken });
+        });
+
         //products
         app.get('/product', async (req, res) => {
             const query = {};
@@ -28,7 +38,27 @@ async function run() {
             const products = await cursor.toArray();
             res.send(products);
         });
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await productCollection.findOne(query);
+            res.send(product);
+        })
 
+        //POST---add
+        app.post('/product', async (req, res) => {
+            const newProduct = req.body;
+            const result = await productCollection.insertOne(newProduct);
+            res.send(result);
+        })
+
+        //DELETE
+        app.delete('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productCollection.deleteOne(query);
+            res.send(result);
+        });
     }
     finally {
 
